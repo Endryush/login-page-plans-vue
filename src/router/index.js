a1861aa0069d891f3fc120ff0a9f3d931b7aac80
Endryush <endryush@gmail.com>
2023-04-14 15:10:08 -0300

    cleaning default project and starting with base colors

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '@/views/LoginPage.vue'
+import Signup from '@/views/SignupPage.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  // {
+  //   path: '/choose-plan',
+  //   name: 'ChoosePlan',
+  //   component: ChoosePlan
+  // },
+  {
+    path:'/:catchAll(.*)',
+    redirect: '/login'
   }
 ]
 
